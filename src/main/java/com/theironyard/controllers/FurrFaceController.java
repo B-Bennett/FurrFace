7daeb0df73f1d32bce570e95bed6ed0d8200a99f
BennettIronYard <bennettiy12@gmail.com>
2015-11-19 16:09:42 -0500

    test init

--- src/main/java/com/theironyard/controllers/FurrFaceController.java
+++ src/main/java/com/theironyard/controllers/FurrFaceController.java
@@ -2,9 +2,24 @@ package com.theironyard.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 /**
  * Created by DrScott on 11/19/15.
  */
 @RestController
 public class FurrFaceController {
+
+  /*  @PostConstruct
+    public void init() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        User user = users.findOneByName("TheMan");
+        if (user == null) {
+            user = new User();
+            user.name = "TheMan";
+            user.password = PasswordHash.createHash("1234");
+            users.save(user);
+        }
+    } */
 }
