72a4936fd4ad676413862e22c0e0c281958e4237
BennettIronYard <bennettiy12@gmail.com>
2015-11-22 16:35:28 -0500

    h

--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -15,7 +15,8 @@ public interface UserRepository extends CrudRepository<User, Integer> {
        List<User> findByPetType(String petType);
        List<User> findAllByPetAge(int petAge);
        User findOneById(int id);
-       List<User> findAllGroupByLikesOrder();
+       //List<User> findAllGroupByLikesOrder();
+       List<User> findByLikeAsc(int likes);
 
 
       // List<User> findAllOrderByPetRatingAsc();
